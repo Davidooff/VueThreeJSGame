34fc4b55df8ca2c209aad78e1f189f99e77b0c6d
Davidooff <david.ter.18@gmail.com>
2025-01-12 01:18:01 +0200

    asd

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -7,36 +7,31 @@ import * as THREE from 'three'
 const props = defineProps<{ game: Game }>()
 
 const cellWetnesMaterials = [
-  new THREE.MeshBasicMaterial({ color: 0xffd500 }),
+  new THREE.MeshBasicMaterial({ color: 0x9e7b1b }),
   new THREE.MeshBasicMaterial({ color: 0x0022ff }),
   new THREE.MeshBasicMaterial({ color: 0x1eff00 }),
 ]
 
-function createBlock(
-  position: [number, number],
-  cellWetnesState: CellWetnesState,
-): THREE.Mesh<THREE.BoxGeometry, THREE.MeshBasicMaterial, THREE.Object3DEventMap> {
-  const geometry = new THREE.BoxGeometry(0.8, 0.8, 0.8)
-  const material = cellWetnesMaterials[cellWetnesState]
-
-  const mesh = new THREE.Mesh(geometry, material)
-  mesh.position.set(position[0], 0, position[1])
-  return mesh
-}
+// function
 
 class GameView {
   width: number
   height: number
   scene = new THREE.Scene()
   renderer = new THREE.WebGLRenderer({ antialias: true })
-  camera = new THREE.PerspectiveCamera(70, 0.01, 10)
+  camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 1000)
+  light = new THREE.DirectionalLight(0xffffff, 1)
   field: FieldData
   fieldView: THREE.Mesh<THREE.BoxGeometry, THREE.MeshBasicMaterial, THREE.Object3DEventMap>[][]
+  player = new THREE.Mesh(
+    new THREE.BoxGeometry(0.2, 0.8, 0.2),
+    new THREE.MeshBasicMaterial({ color: 0xffffff }),
+  )
   constructor(field: FieldData) {
     this.field = field
     //Generating mesh for each cell
     this.fieldView = field.map((row) =>
-      row.map((cell) => createBlock(cell.cellPostion, cell.fieldWetnesState)),
+      row.map((cell) => this.createBlock(cell.cellPostion, cell.fieldWetnesState)),
     )
 
     this.width = window.innerWidth
@@ -48,12 +43,48 @@ class GameView {
         this.scene.add(cell)
       }),
     )
+    //Adding player to the scene
+    this.scene.add(this.player)
+
+    this.camera.position.set(12, 5, 10)
+    this.camera.lookAt(5, 0, 5)
+
+    this.light.position.set(0, 1, 0)
+    this.light.target.position.set(0, 0, 0)
+    this.light.castShadow = true
+    this.scene.add(this.light.target)
+
+    this.player.position.set(this.player.position.x, 0.8, this.player.position.z)
+    this.player.castShadow = true
 
     this.renderer.setSize(this.width, this.height)
+    this.renderer.shadowMap.enabled = true
+    this.renderer.shadowMap.type = THREE.PCFSoftShadowMap
     // renderer.setAnimationLoop(animate)
     document.body.appendChild(this.renderer.domElement)
+    this.renderer.render(this.scene, this.camera)
+  }
+
+  render() {
+    this.renderer.render(this.scene, this.camera)
+  }
+
+  createBlock(
+    position: [number, number],
+    cellWetnesState: CellWetnesState,
+  ): THREE.Mesh<THREE.BoxGeometry, THREE.MeshBasicMaterial, THREE.Object3DEventMap> {
+    const geometry = new THREE.BoxGeometry(0.8, 0.8, 0.8)
+    const material = cellWetnesMaterials[cellWetnesState]
+
+    const mesh = new THREE.Mesh(geometry, material)
+    mesh.position.set(position[0], 0, position[1])
+    mesh.castShadow = true
+    mesh.receiveShadow = true
+    return mesh
   }
 }
+
+const gameView = new GameView(props.game.field.field)
 </script>
 
 <template>
